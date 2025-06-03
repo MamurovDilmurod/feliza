0a4a7a86f574b6f0c044b06447bdf4a87db11069
lazizdev <hamidovlaziz23@gmail.com>
2025-06-03 14:46:15 +0500

    search fixed

--- src/pages/looks/looks-detail.jsx
+++ src/pages/looks/looks-detail.jsx
@@ -3,14 +3,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import ProductCard from "../../components/ProductCart/ProductCard";
 import { Button } from "antd";
 import { IoIosArrowBack } from "react-icons/io";
+import { useTranslation } from "react-i18next";
 
 const LooksDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
+  const { t } = useTranslation();
+
   const { data } = useGetList(
     "/api/lookCollection/getLookCollectionById/" + id
   );
 
-  const navigate = useNavigate();
   const { data: Looks, isLoading } = useGetList(
     "/api/lookCollection/getLookCollection"
   );
@@ -19,30 +22,31 @@ const LooksDetail = () => {
     <div className="lg:space-y-20 px-1 relative">
       <Button
         icon={<IoIosArrowBack />}
-        className="!absolute !left-10 top-10 !text-primary !font-tenor !text-base !border-none backdrop-blur-sm !bg-white/20"
-        children={"Barcha looklar"}
+        className="!absolute !left-3 top-3 lg:!left-10 lg:top-10 !text-primary !font-tenor !text-base !border-none backdrop-blur-sm !bg-white/20"
+        children={t("looks.all")}
+        onClick={() => navigate("/looks")}
       />
       <div className="flex  lg:gap-10 w-full max-w-[1440px] overflow-hidden flex-wrap lg:flex-nowrap">
         <div className="">
           <img
-            className="min-w-[524px] h-[400px] w-full lg:h-[660px] object-cover"
+            className="min-w-[524px] w-full lg:h-[660px] object-cover"
             src={data?.images[0]?.url}
             alt=""
           />
         </div>
 
         <div className="w-full">
-          <div className="flex flex-col gap-2 justify-between h-full py-10">
+          <div className="flex flex-col gap-2 justify-between h-full py-5 lg:py-10">
             <div className="flex justify-between items-center w-full pr-3 lg:pr-20">
               <h1 className="font-tenor font-normal text-xl text-primary">
-                Lookga ega bo'ling!
+                {t("looks.message")}
               </h1>
               <p className="font-tenor font-normal text-base text-primary underline">
                 #felizastyle
               </p>
             </div>
             <p className="font-tenor font-normal text-base text-secondary">
-              {data?.productResponseDtos?.length} mahsulot
+              {data?.productResponseDtos?.length} {t("looks.product")}
             </p>
             <div className="w-full">
               <div
@@ -62,9 +66,9 @@ const LooksDetail = () => {
         </div>
       </div>
 
-      <div className="space-y-10">
+      <div className="space-y-5 lg:space-y-10">
         <h1 className="font-tenor font-normal text-xl text-primary text-center">
-          BOSHQA LOOKLARNI HAM KO’RING
+          {t("looks.smillar")}
         </h1>
 
         <div className={`grid w-full gap-1 grid-cols-2 lg:grid-cols-4`}>
